0540173c9953e29b13b8fb73a27b38e21620cc99
makden <mail@z3x.ru>
2023-07-06 12:28:19 +0300

    Создание таблицы

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <Table />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <hr />
+    <Table :dataTable="TableBody" :headTable="TableHead" />
+    <!--<HelloWorld msg="Tables" /> -->
   </div>
 </template>
 
@@ -15,6 +15,24 @@ export default {
   components: {
     HelloWorld,
     Table,
+  },
+  data() {
+    return {
+      TableHead: [
+        'Заголовок1',
+        'Заголовок2',
+        'Заголовок3',
+        'Заголовок4',
+        'Заголовок5',
+      ],
+      TableBody: [
+        ['val-1-1', 'val-1-2', 'val-1-3', 'val-1-4', 'val-1-5'],
+        ['val-2-1', 'val-2-2', 'val-2-3', 'val-2-4', 'val-2-5'],
+        ['val-3-1', 'val-3-2', 'val-3-3', 'val-3-4', 'val-3-5'],
+        ['val-4-1', 'val-4-2', 'val-4-3', 'val-4-4', 'val-4-5'],
+        ['val-5-1', 'val-5-2', 'val-5-3', 'val-5-4', 'val-5-5'],
+      ],
+    };
   },
 };
 </script>
--- src/components/table.vue
+++ src/components/table.vue
@@ -1,12 +1,75 @@
-<template>dfdff</template>
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th v-for="col in headTable">{{ col }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="rows in dataTable">
+        <td v-for="col in rows">{{ col }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
 
 <script>
 export default {
   name: 'table',
   props: {
-    msg: String,
+    dataTable: Array,
+    headTable: Array,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.table {
+  width: 100%;
+  border: none;
+  margin-bottom: 20px;
+  border-collapse: separate;
+}
+.table thead th {
+  font-weight: bold;
+  text-align: left;
+  border: none;
+  padding: 10px 15px;
+  background: #def;
+  font-size: 14px;
+  border-top: 1px solid #ddd;
+}
+.table tr th:first-child,
+.table tr td:first-child {
+  border-left: 1px solid #ddd;
+}
+.table tr th:last-child,
+.table tr td:last-child {
+  border-right: 1px solid #ddd;
+}
+.table thead tr th:first-child {
+  border-radius: 10px 0 0 0;
+}
+.table thead tr th:last-child {
+  border-radius: 0 10px 0 0;
+}
+.table tbody td {
+  text-align: left;
+  border: none;
+  padding: 10px 15px;
+  font-size: 14px;
+  vertical-align: top;
+}
+.table tbody tr:nth-child(even) {
+  background: #f8f8f8;
+}
+.table tbody tr:last-child td {
+  border-bottom: 1px solid #ddd;
+}
+.table tbody tr:last-child td:first-child {
+  border-radius: 0 0 0 20px;
+}
+.table tbody tr:last-child td:last-child {
+  border-radius: 0 0 20px 0;
+}
+</style>
